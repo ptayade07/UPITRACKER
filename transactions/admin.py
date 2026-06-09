4ffb6e5010bff2708e307fd7e3cb4b4fb83319da
Purva Tayade <pptayade07@gmail.com>
2026-06-09 14:07:45 +0530

    Register Transaction in admin panel, add superuser

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['date', 'amount', 'transaction_type', 'payment_app', 'merchant', 'category']
+    list_filter = ['transaction_type', 'payment_app', 'category']
+    search_fields = ['merchant', 'upi_ref', 'raw_sms']
