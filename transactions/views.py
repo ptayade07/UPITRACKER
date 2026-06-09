9d3017601afb1642e773b64f65916836c2bb7877
Purva Tayade <pptayade07@gmail.com>
2026-06-09 14:51:37 +0530

    Add transaction list page with filters

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Transaction
 
-# Create your views here.
+@login_required
+def transaction_list(request):
+    transactions = Transaction.objects.filter(user=request.user)
+    
+    # Filters
+    payment_app = request.GET.get('payment_app')
+    transaction_type = request.GET.get('transaction_type')
+    
+    if payment_app:
+        transactions = transactions.filter(payment_app=payment_app)
+    if transaction_type:
+        transactions = transactions.filter(transaction_type=transaction_type)
+    
+    context = {
+        'transactions': transactions,
+        'payment_app': payment_app,
+        'transaction_type': transaction_type,
+    }
+    return render(request, 'transactions/transaction_list.html', context)
